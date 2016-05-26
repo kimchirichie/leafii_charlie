b901dbe31aad1b9ac82d54577a2322c1b2dd0c7f
Talent Zeng <talent.z@hotmail.com>
2016-05-26 18:23:27 -0400

    got rid of .map files

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,13 +12,15 @@ module.exports = function(grunt) {
 
 		sass: {
 			dist: {
-				files: {
-					'public/stylesheets/admin.css' : 'public/stylesheets/scss/admin.scss',
-					'public/stylesheets/animate.css' : 'public/stylesheets/scss/animate.scss',
-					'public/stylesheets/auth.css' : 'public/stylesheets/scss/auth.scss',
-					'public/stylesheets/landing.css' : 'public/stylesheets/scss/landing.scss',
-					'public/stylesheets/profile.css' : 'public/stylesheets/scss/profile.scss'
-				}
+				files: [{
+					sourcemap: false,
+					expand: true,
+					cwd:'stylesheets',
+					src: ['*.scss'],
+					dest: '../public/stylesheets',
+					ext: '.css'
+					// 'public/stylesheets/admin.css' : 'public/stylesheets/scss/admin.scss',
+				}]
 			}
 		},
 
