19453e744cb9b017a585498bf7eeb30707f46b7d
Talent Zeng <talent.z@hotmail.com>
2016-05-29 18:05:52 -0400

    grunt fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,22 +3,41 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		watch: {
-			files: ['*'],
-			tasks: ['nodemon', 'sass'],
+			// files: ['*'],
+			// tasks: ['nodemon', 'sass:dev'],
+
+			css: {
+				files: ['public/stylesheets/scss/*.scss'],
+				tasks: ['sass:dev'],
+			},
+
 			options: {
 				spawn: false,
 			},
 		},
 
 		sass: {
-			dist: {
+			dev: {
+				options: {
+					sourcemap: false,
+					style: 'expanded'
+				},
 				files: {
-					// sourcemap: false,
-					// expand: true,
-					// cwd:'stylesheets/css',
-					// src: ['*.scss'],
-					// dest: '../public/stylesheets/css',
-					// ext: '.css'
+					'public/stylesheets/css/admin.css' : 'public/stylesheets/scss/admin.scss',
+					'public/stylesheets/css/auth.css' : 'public/stylesheets/scss/auth.scss',
+					'public/stylesheets/css/thankyou.css' : 'public/stylesheets/scss/thankyou.scss',
+					'public/stylesheets/css/landing.css' : 'public/stylesheets/scss/landing.scss',
+					'public/stylesheets/css/profile.css' : 'public/stylesheets/scss/profile.scss',
+					'public/stylesheets/css/animate.css' : 'public/stylesheets/scss/animate.scss'
+				}
+			},
+
+			build: {
+				options: {
+					sourcemap: false,
+					style: 'compressed'
+				},
+				files: {
 					'public/stylesheets/css/admin.css' : 'public/stylesheets/scss/admin.scss',
 					'public/stylesheets/css/auth.css' : 'public/stylesheets/scss/auth.scss',
 					'public/stylesheets/css/thankyou.css' : 'public/stylesheets/scss/thankyou.scss',
@@ -27,19 +46,21 @@ module.exports = function(grunt) {
 					'public/stylesheets/css/animate.css' : 'public/stylesheets/scss/animate.scss'
 				}
 			}
+
 		},
 
 		nodemon: {
-				dev: {
-					script: './bin/www'
-				},
+			dev: {
+				script: './bin/www'
+			},
 		},
 	});
 
 	grunt.loadNpmTasks('grunt-nodemon');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-sass');
 	
 	// Default task(s).
-	grunt.registerTask('default', ['sass','nodemon','watch']);
+	grunt.registerTask('default', ['sass:dev','nodemon','watch']);
+	grunt.registerTask('build', ['nodemon', 'watch','sass:build']);
 };
